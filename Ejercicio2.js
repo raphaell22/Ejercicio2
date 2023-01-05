2d40e95469b1034c842cb02adec995041dafb73a
rafael22 <rafaelcarmona295@hmail.com>
2023-01-04 22:04:05 -0400

    Subiendo

---
+++ Ejercicio2.js
@@ -0,0 +1,14 @@
+const libro = {
+    titulo : "Una vida con proposito",
+    autor : " Rick Warren",
+    fecha :2002,
+    url : "no la tengo "
+
+}
+
+
+const nacimiento= new Date("22 January 1995");
+
+
+const array = ["Rafael ", 27,false, nacimiento, libro];
+console.log(array);
